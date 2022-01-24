bcb60309bfe5b8e30c70fac17637bd8ecceda9dc
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-01-24 18:55:52 +0200

    app 1.2

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,3 +5,4 @@ export * from './CurrentRates/CurrentRates';
 export * from './Currency/Currency';
 export * from './Exchange/Exchange';
 export * from './ConverterForm/ConverterForm';
+export * from './Loader/Loader';
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,23 +2,27 @@ import React, {useEffect, useState} from 'react';
 
 import css from './homePage.module.css';
 import {currencyService} from "../../services";
-import {CurrentRates, Exchange} from "../../Components";
+import {CurrentRates, Exchange, Loader} from "../../Components";
 
 const HomePage = () => {
 
-    const [rates, setRates] = useState([])
+    const [rates, setRates] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
+    useEffect(() => {
         currencyService.getCurrentRates()
-            .then(response => setRates([...response]))
+            .then(response => {
+                setRates([...response]);
+                setLoading(false);
+            })
     }, [])
 
     return (
         <div className='main'>
-            <div className={css.mainContainer}>
+            {loading ? <Loader/> : <div className={css.mainContainer}>
                 <CurrentRates rates={rates}/>
                 <Exchange rates={rates}/>
-            </div>
+            </div>}
         </div>
     );
 };
